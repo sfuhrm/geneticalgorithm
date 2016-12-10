41497d00abc534e2995fe97af6dffb4c4123a9e9
Stephan Fuhrmann <s_fuhrm@gmx.de>
2016-12-10 13:27:32 +0100

    Added package info

---
+++ src/main/java/com/laekery/genetic/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016 Stephan Fuhrmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** 
+ * Base classes for a genetic algorithm.
+ * The {@link AbstractHypothesis} class must be derived and implemented
+ * to model your problem. The {@link GeneticAlgorithm} class is the
+ * genetic algorithm implementation which is able to calculate the
+ * maximum after a defined number of iterations.
+ * @author Stephan Fuhrmann
+ */
+package com.laekery.genetic;
