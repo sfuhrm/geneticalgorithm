298fe7babf4dd8734915c0baa3336dc473974b04
Stephan Fuhrmann <s_fuhrm@gmx.de>
2016-12-10 13:02:39 +0100

    Adjusted tests to new fitness function

--- src/test/java/com/laekery/genetic/example/IntGuessingHypothesisTest.java
+++ src/test/java/com/laekery/genetic/example/IntGuessingHypothesisTest.java
@@ -38,20 +38,27 @@ public class IntGuessingHypothesisTest {
     public void testFitnessWithGoal() {
         IntGuessingHypothesis a = new IntGuessingHypothesis(4);
         a.setGenome(new int[] {0,1,2,3});
-        assertEquals(0, a.calculateFitness(), 0.001);
+        assertEquals(4., a.calculateFitness(), 0.001);
+    }
+    
+    @Test
+    public void testFitnessWithSingleHit() {
+        IntGuessingHypothesis a = new IntGuessingHypothesis(4);
+        a.setGenome(new int[] {0,9,9,9});
+        assertEquals(1., a.calculateFitness(), 0.001);
     }
     
     @Test
     public void testFitnessWithQuadMiss() {
         IntGuessingHypothesis a = new IntGuessingHypothesis(4);
         a.setGenome(new int[] {1,2,3,4});
-        assertEquals(-4, a.calculateFitness(), 0.001);
+        assertEquals(0., a.calculateFitness(), 0.001);
     }
     
     @Test
     public void testFitnessWithFullMiss() {
         IntGuessingHypothesis a = new IntGuessingHypothesis(4);
         a.setGenome(new int[] {3,2,1,0});
-        assertEquals(-(3+1+1+3), a.calculateFitness(), 0.001);
+        assertEquals(0., a.calculateFitness(), 0.001);
     }
 }
