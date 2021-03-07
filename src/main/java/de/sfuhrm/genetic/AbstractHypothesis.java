8aa38274570ae4fd9efc49ca5e2cc2273bbfd33f
Stephan Fuhrmann <s@sfuhrm.de>
2021-03-07 14:25:34 +0100

    Javadoc

--- src/main/java/de/sfuhrm/genetic/AbstractHypothesis.java
+++ src/main/java/de/sfuhrm/genetic/AbstractHypothesis.java
@@ -17,11 +17,18 @@ package de.sfuhrm.genetic;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 /** Hypothesis base class.
+ * Your implementation can rely on that the {@linkplain #randomInit()} method
+ * is used after calling the constructor.
+ *
  * @param <T> the parameter needs to have the concrete type of the
  *           hypothesis class for the methods to return the
  *           correct subtype.
@@ -29,18 +36,22 @@ import lombok.Setter;
  */
 public abstract class AbstractHypothesis<T extends AbstractHypothesis<?>> {
 
-    /** Fitness of this hypothesis. */
+    /** Fitness of this hypothesis.
+     * Used by {@linkplain GeneticAlgorithm} internally.
+     * */
     @Getter(AccessLevel.PACKAGE) @Setter(AccessLevel.PACKAGE)
     private double fitness;
 
-    /** Selection probability based on energy of this hypothesis. */
+    /** Selection probability based on energy of this hypothesis.
+     * Used by {@linkplain GeneticAlgorithm} internally.
+     * */
     @Getter(AccessLevel.PACKAGE) @Setter(AccessLevel.PACKAGE)
     private double probability;
 
     /** The source of randomness that can be used in the hypothesis.
      */
     @Getter(AccessLevel.PROTECTED)
-    private final Random random = new Random();
+    private final static Random RANDOM = new Random();
 
     /** Randomly initialize the hypothesis.
      * @return returns {@code this} hypothesis after a random initialization.
@@ -48,20 +59,34 @@ public abstract class AbstractHypothesis<T extends AbstractHypothesis<?>> {
     protected abstract T randomInit();
 
     /** Returns a two-element crossover offspring of this and the
-     * other hypothesis.
+     * other hypothesis. The two offsprings are a combination of
+     * {@code this} and the {@code other} instances. Example:
+     * if the parents are the vectors {@code (a,b)} and {@code (c,d)},
+     * possible cross over children could be
+     * {@code (a,d)} and {@code (c,b)}.
      * @param other the other hypothesis to cross-over with.
      * @return a two-element List of the offsprings.
+     * @see #mutate() the other genetic operator on this instance.
      */
     protected abstract List<T> crossOver(T other);
 
-    /** Calculates the fitness of this hypothesis.
+    /** Implements the mutation operator on this hypothesis.
+     * The mutation operator flips one bit
+     * of the genome of the hypothesis. Example: If a hypothesis consists
+     * of the vector {@code (a,b,c)}, then a possible mutation result
+     * could be {@code (a,d,c)} or {@code (d,b,c)}.
+     * @see #crossOver(AbstractHypothesis) the other genetic operator on this instance.
+     */
+    protected abstract void mutate();
+
+    /** Calculates the fitness of this hypothesis. A bigger result means a
+     * better performance of this hypothesis. The winner hypothesis will
+     * be the one with a high fitness function result.
+     * If calculating the fitness function is expensive, consider
+     * using he {@linkplain GeneticAlgorithm#findMaximum(Function, Supplier, ExecutorService)}
+     * parallel approach to have a higher throughput.
      * @return a fitness where a bigger number means more fitness. Please
      * be advised that negative numbers will be problematic for the algorithm.
      */
     protected abstract double calculateFitness();
-
-    /** Implements the mutation operator. The mutation operator flips one bit
-     * of the genome of the hypothesis.
-     */
-    protected abstract void mutate();
 }
