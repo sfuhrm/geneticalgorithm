8aa03279f89d6b53b4e5c80a947e00ae58b109e6
Stephan Fuhrmann <s@sfuhrm.de>
2021-03-07 13:32:32 +0100

    Unused

--- src/main/java/de/sfuhrm/genetic/GeneticAlgorithm.java
+++ src/main/java/de/sfuhrm/genetic/GeneticAlgorithm.java
@@ -18,7 +18,6 @@ package de.sfuhrm.genetic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
