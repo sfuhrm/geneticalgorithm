7778d1ff148a46e84169795fb8b0f1de8c465b22
Stephan Fuhrmann <s@sfuhrm.de>
2022-04-30 20:21:11 +0200

    Rename params

--- geneticalgorithm/src/main/java/de/sfuhrm/genetic/GeneticAlgorithmBuilder.java
+++ geneticalgorithm/src/main/java/de/sfuhrm/genetic/GeneticAlgorithmBuilder.java
@@ -93,78 +93,78 @@ public class GeneticAlgorithmBuilder<H> {
 
     /**
      * Defines the cross-over rate to use.
-     * @param set the cross-over rate between 0 and 1.
+     * @param inRate the cross-over rate between 0 and 1.
      * @return this instance.
      * @throws IllegalArgumentException if the valid parameter range
      * is exceeded.
      * */
     public GeneticAlgorithmBuilder<H> withCrossOverRate(
-            final double set) {
-        if (set < 0. || set > 1.) {
+            final double inRate) {
+        if (inRate < 0. || inRate > 1.) {
             throw new IllegalArgumentException(
-                    "Input must be x >= 0 && x >= 1, but is " + set);
+                    "Input must be x >= 0 && x >= 1, but is " + inRate);
         }
-        this.crossOverRate = set;
+        this.crossOverRate = inRate;
         return this;
     }
 
     /**
      * Defines the random generator to use.
-     * @param set the random generator to use.
+     * @param inRandom the random generator to use.
      * @return this instance.
      * @throws IllegalArgumentException if the valid parameter range
      * is exceeded.
      * */
     public GeneticAlgorithmBuilder<H> withRandom(
-            @NonNull final Random set) {
-        this.random = set;
+            @NonNull final Random inRandom) {
+        this.random = inRandom;
         return this;
     }
 
     /**
      * Defines the executor service to use.
-     * @param set the executor service.
+     * @param inExecutorService the executor service.
      * @return this instance.
-     * @throws IllegalArgumentException if the valid parameter range
-     * is exceeded.
+     * @throws NullPointerException if the input
+     * reference is {@code null}.
      * */
     public GeneticAlgorithmBuilder<H> withExecutorService(
-            @NonNull final ExecutorService set) {
-        this.executorService = set;
+            @NonNull final ExecutorService inExecutorService) {
+        this.executorService = inExecutorService;
         return this;
     }
 
     /**
      * Defines the mutation rate to use.
-     * @param set the mutation rate between 0 and 1.
+     * @param rate the mutation rate between 0 and 1.
      * @return this instance.
      * @throws IllegalArgumentException if the valid parameter range
      * is exceeded.
      * */
     public GeneticAlgorithmBuilder<H> withMutationRate(
-            final double set) {
-        if (set < 0. || set > 1.) {
+            final double rate) {
+        if (rate < 0. || rate > 1.) {
             throw new IllegalArgumentException(
-                    "Input must be x >= 0 && x >= 1, but is " + set);
+                    "Input must be x >= 0 && x >= 1, but is " + rate);
         }
-        this.mutationRate = set;
+        this.mutationRate = rate;
         return this;
     }
 
     /**
      * Defines the generation size to use.
-     * @param set the generation size greater or equal to 2.
+     * @param size the generation size greater or equal to 2.
      * @return this instance.
      * @throws IllegalArgumentException if the valid parameter range
      * is exceeded.
      * */
     public GeneticAlgorithmBuilder<H> withGenerationSize(
-            final int set) {
-        if (set < 2) {
+            final int size) {
+        if (size < 2) {
             throw new IllegalArgumentException(
-                    "Input must be x >= 2, but is " + set);
+                    "Input must be x >= 2, but is " + size);
         }
-        this.generationSize = set;
+        this.generationSize = size;
         return this;
     }
 
